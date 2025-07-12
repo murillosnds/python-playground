5b90438f737845bde5c61f1ad2c70e9844a8ec71
Mubru <murillosergionsousa@outlook.com>
2025-07-11 22:07:23 -0300

    Update 6 - Matemática.py

--- Básico/6 - Matemática.py
+++ Básico/6 - Matemática.py
@@ -1,12 +1,13 @@
 numero_1 = int(float(input("Digite um número: ")))
 numero_2 = int(float(input("Digite um número: ")))
-tipo = input("Qual tipo de operação deseja fazer? (+,-,*,/,**)")
+tipo = input("Qual tipo de operação deseja fazer? ( + | - | * | / | ** | % ): ")
 
 soma = numero_1 + numero_2
 subtração = numero_1 - numero_2
 multiplicação = numero_1 * numero_2
 divisão = numero_1 / numero_2
 potenciação = numero_1 ** numero_2
+resto_da_divisao = numero_1 % numero_2
 
 if tipo == "+":
     print(f"O resultado de {numero_1} + {numero_2} é {soma}.")
@@ -18,6 +19,8 @@ elif tipo == "/":
     print(f"O resultado de {numero_1} / {numero_2} é {divisão}.")
 elif tipo == "**":
     print(f"O resultado de {numero_1} ** {numero_2} é {potenciação}.")
+elif tipo == "%":
+    print(f"O resultado do resto da divisão de {numero_1} / {numero_2} é {resto_da_divisao}.")
 
 # Em python é possível realizar contas matématicas como adição, subtração, divisão, multiplicação e potenciação.
 
