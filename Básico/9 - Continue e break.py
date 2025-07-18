7cf2c972e1e197c5323f50bb5f35a3528bed1ecd
Mubru <murillosergionsousa@outlook.com>
2025-07-18 19:11:37 -0300

    Update 9 - Continue e break.py

---
+++ Básico/9 - Continue e break.py
@@ -0,0 +1,10 @@
+seguidores = 1
+while seguidores < 100:
+    seguidores += 1
+    if seguidores == 50:
+        continue
+    print(seguidores)
+
+# Aqui usamos while (enquanto) e continue (continuar). A variável seguidores armazena o valor 1 e enquanto o números de seguidores for menor que 100 é somado de 1 em 1. Se o número de seguidores for igual a 50, o código continuará.
+
+# Por fim é impresso o número de seguidores.
