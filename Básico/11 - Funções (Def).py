716449e11ad0c0017070758e92d130e093eb6602
Mubru <murillosergionsousa@outlook.com>
2025-07-18 20:17:22 -0300

    1 + 1 = 2

--- Básico/11 - Funções (Def).py
+++ Básico/11 - Funções (Def).py
@@ -4,4 +4,6 @@ def soma(n1,n2):
 
 soma(1,1)
 
-#
+# Nesse exemplo temos a função (def) soma com n1 e n2. A variável somando pega os valores de n1 e n2 e soma.
+
+# Por fim imprime o valor da operação de 1 + 1 = 2.
