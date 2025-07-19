4228a6a12d916049c0bd62a9bff873b6f2caa658
Mubru <murillosergionsousa@outlook.com>
2025-07-18 21:23:31 -0300

    atualização

--- Básico/10_funcoes_range.py
+++ Básico/10_funcoes_range.py
@@ -1,11 +1,11 @@
 """Aprendendo o que são funções 1/2."""
 
-n = list(range(1,10,1))
+n = list(range(1,10,1)) 
 print(n)
 
-# Neste exemplo a variável n guarda o valor da list com uma função range (intervalo ou sequência).
+"""# Por fim é impresso uma lista de 1 a 9. Começa no 1 para no 9 de um em um."""
 
-# Por fim é impresso uma lista de 1 a 9. Começa no 1 para no 9 de um em um.
+# Neste exemplo a variável n guarda o valor da list com uma função range (intervalo ou sequência).
 
 # O range funciona dessa forma: range(start, stop, step) ou range(inicio, fim, incremento).
 
