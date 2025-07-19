9c6bde068f9a805923b290e3cc4d0bc2082d5577
Mubru <murillosergionsousa@outlook.com>
2025-07-18 21:25:19 -0300

    atualização

--- Básico/10_funcoes_range.py
+++ Básico/10_funcoes_range.py
@@ -3,10 +3,13 @@
 n = list(range(1,10,1)) 
 print(n)
 
-"""# Por fim é impresso uma lista de 1 a 9. Começa no 1 para no 9 de um em um."""
 
 # Neste exemplo a variável n guarda o valor da list com uma função range (intervalo ou sequência).
 
+# Por fim imprime o valor da operação de 1 + 1 = 2.
+
 # O range funciona dessa forma: range(start, stop, step) ou range(inicio, fim, incremento).
+
+"""Por fim imprime o valor da operação de 1 + 1 = 2."""
 
 # Vamos continuar abordando as funções.
